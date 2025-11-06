c1e7385b1394e55655ab2370252c1b68408a5011
Rino Godson <rinogodson@gmail.com>
2025-11-06 10:45:45 +0530

    added the logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="w-screen h-screen bg-[#090E13] flex justify-center items-start p-10">
+      <img src="/pixecho.svg" className="w-50" />
+    </div>
+  );
 }
 
 export default App;
